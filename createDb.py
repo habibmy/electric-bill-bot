b607533808fa39c6cfba06027603aba48e54252e
habibmy <habib.my@rediffmail.com>
2023-11-12 23:18:47 +0530

    script to create dbs

---
+++ createDb.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect('bill.db')
+
+try:
+    c = conn.cursor()
+
+    bills_table ="""
+    CREATE TABLE IF NOT EXISTS bills (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ca_number TEXT NOT NULL,
+    bill_details TEXT NOT NULL
+    )"""
+
+    notification_Table ="""
+    CREATE TABLE IF NOT EXISTS notification_settings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ca_number TEXT NOT NULL,
+        telegram_chat_ids TEXT,
+        whatsapp_phone_numbers TEXT
+    )"""
+
+    c.execute(bills_table)
+
+    c.execute(notification_Table)
+    print("table created")
+except:
+    print("table not created")
+
+finally:
+    conn.close()       
